ab08805e6c71435063f6c64c769bbaf2f28a23f2
Neda Kušurin <nk51342@fer.hr>
2020-11-05 17:19:52 +0100

    [#12] Add basic homepage 2

--- IzvorniKod/frontend/src/components/HomePage.tsx
+++ IzvorniKod/frontend/src/components/HomePage.tsx
@@ -2,32 +2,39 @@ import React from "react";
 import './HomePage.css';
 import './App.css';
 
-import logo from "./pictures/slika1.png";
-import logo2 from "./pictures/lqbc14.jpg";
 
 export const HomePage = () => {
 
     return (
-        <div>
 
-            <header className="header">
-                <a className="header-link" href ="/">logo_slika</a>
-                <h1 className="heading">Planinarski dnevnik</h1>
-                <a className="header-link"  href="/">slika profila</a>
-            </header>
+        <body>
+        <header className="header">
+            <a className="header-link" href ="/">logo_slika</a>
+            <h1 className="heading">Planinarski dnevnik</h1>
+            <a className="header-link"  href="/">slika profila</a>
+        </header>
 
-            <div>Najljepša stranica na internetu :)</div>
-
-            <div className="footer">
-
-                <div>Otkrili ste novi planinarski dom?</div>
-                <div>Naišli ste na neispravne informacije?</div>
-                <div>Pošaljite poruku našim administratorima</div>
-                <div>i pomozite održati stranicu ažurnom!</div>
-                <a href="/"> KONTAKTIRAJ ADMNISTRATORA </a>
+        <main className="main">
+            <div className="main-images">
+                <div className="staze">
+                    <a className="pretraga" href="#" >Pretražite staze</a>
+                </div>
+                <div className="domovi">
+                    <a className="pretraga" href="#" >Pretražite planinarske domove</a>
+                </div>
             </div>
+        </main>
 
-        </div>
+        <footer className="footer">
+
+            <div>Otkrili ste novi planinarski dom?</div>
+            <div>Naišli ste na neispravne informacije?</div>
+            <div>Pošaljite poruku našim administratorima</div>
+            <div>i pomozite održati stranicu ažurnom!</div>
+            <a href="/"> KONTAKTIRAJ ADMNISTRATORA </a>
+        </footer>
+
+        </body>
 
     );
 
