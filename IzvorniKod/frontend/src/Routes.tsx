50e67fb8a473e1cd4b959acad5cb137ca8dc4943
Ivan Martinović <ivan.martinovic@fer.hr>
2020-11-07 23:21:58 +0100

    [#10] Fix css on registration and login form. Add image loader on registration

--- IzvorniKod/frontend/src/Routes.tsx
+++ IzvorniKod/frontend/src/Routes.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {Redirect, Route, Switch} from "react-router";
 import {HomePage} from "./components/HomePage";
 import {AboutUs} from "./aboutus/components/AboutUs/AboutUs";
-import {MountainLodgeSearch} from "./components/search/MountainLodgeSearch";
 import Header from "./components/Header";
 import {RegistrationForm} from "./components/RegistrationForm";
 import {LoginForm} from "./components/LoginForm";
@@ -20,9 +19,6 @@ export const Routes = () => {
                 <Route path={"/aboutus"} component={AboutUs} exact={true}/>
                 <Route path="/registracija" component={RegistrationForm} exact={true}/>
                 <Route path="/prijava" component={LoginForm} exact={true}/>
-                <Route path="/mountain-lodge/search" exact>
-                    <MountainLodgeSearch/>
-                </Route>
             </Switch>
         </div>
     );
