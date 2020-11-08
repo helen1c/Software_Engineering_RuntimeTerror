dd3a2014506f4e03913c9c6dd5741be69321f9a6
Ivan Martinović <ivan.martinovic@fer.hr>
2020-11-08 23:19:10 +0100

    [#12] Rename pictures

--- IzvorniKod/frontend/src/components/HomePage.tsx
+++ IzvorniKod/frontend/src/components/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import './HomePage.css';
-import './App.css';
+import './css/HomePage.css';
+import '../App.css';
 
 
 export const HomePage = () => {
@@ -8,11 +8,6 @@ export const HomePage = () => {
     return (
 
         <body>
-        <header className="header">
-            <a className="header-link" href ="/">logo_slika</a>
-            <h1 className="heading">Planinarski dnevnik</h1>
-            <a className="header-link"  href="/">slika profila</a>
-        </header>
 
         <main className="main">
             <div className="main-images">
@@ -24,15 +19,6 @@ export const HomePage = () => {
                 </div>
             </div>
         </main>
-
-        <footer className="footer">
-
-            <div>Otkrili ste novi planinarski dom?</div>
-            <div>Naišli ste na neispravne informacije?</div>
-            <div>Pošaljite poruku našim administratorima</div>
-            <div>i pomozite održati stranicu ažurnom!</div>
-            <a href="/"> KONTAKTIRAJ ADMNISTRATORA </a>
-        </footer>
 
         </body>
 
