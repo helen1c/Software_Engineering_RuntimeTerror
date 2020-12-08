7ca34db27975be2eb53b40b716061be6685db8e2
Neda Kušurin <nk51342@fer.hr>
2020-12-08 15:39:33 +0100

    [#16] Search mountaineering community 2

--- IzvorniKod/frontend/src/components/mountaineering-community/MountaineeringCommunitySearch.tsx
+++ IzvorniKod/frontend/src/components/mountaineering-community/MountaineeringCommunitySearch.tsx
@@ -1,51 +1,22 @@
-import React, {useState} from "react";
-import {InputLabel, TextField} from "@material-ui/core";
-import './MountaineeringCommunitySearch.css'
-import Select from '@material-ui/core/Select';
-import MenuItem from "@material-ui/core/MenuItem";
-import {useFormik} from "formik";
-import * as Yup from "yup";
-import {HttpCodesUtil} from "../../errors/HttpCodesUtil";
+import React, { useEffect, useState } from "react";
+import { SearchBar } from "./SearchBar";
+import { UserInfo } from "../mountain-lodge/models/UserInfo";
 
 
 export const MountaineeringCommunitySearch = () => {
 
-    const formik = useFormik({
-        initialValues: {
-            name: ""
-        },
-        onSubmit: (values) => {
-
-            fetch("/api/users/community", { //backend
-                method: "POST",
-                body: JSON.stringify(values),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then((response) => {
-                if (response.status !== HttpCodesUtil.SUCCESS) {
-                    response.text().then((errors) => {
-                        errors = JSON.parse(errors);
-                        Object.values(errors).forEach((value) => {
-                            const error = JSON.parse(JSON.stringify(value));
-                            formik.setFieldError(error.fieldName, error.message);
-                        });
-                    });
-                } else {
-                    //GRESKA
-                }
-            });
-        },
-    });
-
+    const [users, setUsers] = useState<UserInfo[]>([]);
 
     return (
+        <div>
+            <SearchBar dataFunction={setUsers} />
+            {!users.length ? (
+                <div className="text-3xl mt-12">Ne postoji niti jedan korisnik koji odgovara zadanom parametru.</div>
+            ) : (
+                //<UserInfo[] user={users} />
 
-        <div className="search-container">
-            Moja planinarska zajednica:
-            <TextField name="search_text"  placeholder="Pretraži" className="search-field"  type="search" variant="outlined" />
+                <p>Ima korisnika</p>
+            )}
         </div>
-
     );
-
-}
+};
