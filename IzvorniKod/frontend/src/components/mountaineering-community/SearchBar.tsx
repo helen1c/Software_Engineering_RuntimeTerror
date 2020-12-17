d0d7ea19a55fc320c22d5caab6e4d032649abc18
Ivan Martinović <ivan.martinovic@fer.hr>
2020-12-17 13:18:53 +0100

    Resolve

--- IzvorniKod/frontend/src/components/mountaineering-community/SearchBar.tsx
+++ IzvorniKod/frontend/src/components/mountaineering-community/SearchBar.tsx
@@ -1,70 +1,70 @@
-import React, { useEffect } from "react";
-import { useFormik } from "formik";
+import React from "react";
+import {useFormik} from "formik";
 
-import { UserInfo } from "../mountain-lodge/models/UserInfo";
-import { HttpCodesUtil } from "./HttpCodesUtil";
+import {UserInfo} from "../mountain-lodge/models/UserInfo";
+import {HttpCodesUtil} from "./HttpCodesUtil";
 
 interface Props {
-  dataFunction: (userArray: UserInfo[]) => void;
+    dataFunction: (userArray: UserInfo[]) => void;
 }
 
-export const SearchBar = ({ dataFunction }: Props) => {
-  const dataRequest = (values: any) => {
-    fetch("/api/users/community?name=" + values, {
-      method: "GET",
-      headers: new Headers({
-        authorization: sessionStorage.getItem("key") || "",
-      }),
-    }).then(function (response) {
-      if (response.status === HttpCodesUtil.SUCCESS) {
-        response.json().then((users) => {
-          dataFunction(users);
+export const SearchBar = ({dataFunction}: Props) => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    function dataRequest(values: String) {
+        fetch("/api/users/community?name=" + values, {
+            method: "GET",
+            headers: new Headers({
+                authorization: sessionStorage.getItem("key") || "",
+            }),
+        }).then(function (response) {
+            if (response.status === HttpCodesUtil.SUCCESS) {
+                response.json().then((users) => {
+                    users.forEach(function (item: UserInfo) {
+                        item.image = "data:image/jpeg;base64," + item.image;
+                    });
+                    dataFunction(users);
+                });
+            } else {
+                dataFunction([]);
+            }
         });
-      } else {
-        dataFunction([]);
-      }
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            searchText: "",
+        },
+        onSubmit: (values) => {
+            dataRequest(values.searchText);
+        },
     });
-  };
 
-  useEffect(() => {
-    dataRequest("");
-  }, []);
+    const placeholder = () => {
+        return formik.values.searchText === ""
+            ? "Unesi ključnu riječ"
+            : "Prvi korisnik, drugi korisnik, treći korisnik,...";
+    };
 
-  const formik = useFormik({
-    initialValues: {
-      searchText: "",
-    },
-    onSubmit: (values) => {
-      dataRequest(values.searchText);
-    },
-  });
+    function handleSearchChanged(e: any) {
+        formik.handleChange(e);
+        dataRequest(e.target.value);
+    }
 
-  const placeholder = () => {
-    return formik.values.searchText === ""
-      ? "Unesi ključnu riječ"
-      : "Prvi korisnik, drugi korisnik, treći korisnik,...";
-  };
-
-  function handleSearchChanged(e: any) {
-    formik.handleChange(e);
-    dataRequest(e.target.value);
-  }
-
-  return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
+    return (
         <div>
-          <div>
-            <input
-              type="text"
-              name="searchText"
-              placeholder={placeholder()}
-              onChange={(e) => handleSearchChanged(e)}
-              value={formik.values.searchText}
-            />
-          </div>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <div>
+                        <input
+                            type="text"
+                            name="searchText"
+                            placeholder={placeholder()}
+                            onChange={(e) => handleSearchChanged(e)}
+                            value={formik.values.searchText}
+                        />
+                    </div>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
