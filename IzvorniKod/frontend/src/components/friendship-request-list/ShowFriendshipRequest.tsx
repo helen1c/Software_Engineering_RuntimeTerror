d73d707619cc0dfb3ee5ff5c158bc8c33f5a3886
Neda Kušurin <nk51342@fer.hr>
2020-12-30 00:22:40 +0100

    [#53] Added buttons for confirming and refusing friendship request

--- IzvorniKod/frontend/src/components/friendship-request-list/ShowFriendshipRequest.tsx
+++ IzvorniKod/frontend/src/components/friendship-request-list/ShowFriendshipRequest.tsx
@@ -1,28 +1,48 @@
 import {UserInfo} from "../mountain-lodge/models/UserInfo";
 import "./ShowFriendshipRequest.css"
-import React from "react";
+import React, {useState} from "react";
+import {useHistory} from "react-router";
 
 interface Props {
     user: UserInfo;
 }
 
 export const ShowFriendshipRequest= ({ user }: Props) => {
+    const history = useHistory();
+    const [isConfirmed, setIsConfirmed] = useState<boolean>(false);
+    const [isRefused, setIsRefused] = useState<boolean>(false);
+
+    function confirm() {
+        setIsConfirmed(true);
+        //staviti u listu prijatelja
+    }
+    function refuse() {
+        setIsRefused(true);
+    }
 
     return (
+
         <div className="users-container">
+            {!isConfirmed && !isRefused?(
             <div>
                  <img
                     alt={user.name}
                     src={user.image}
                     className="user-photo"
+                    onClick={(e) => history.push("/profile/" + user.id)}
                  />
-                <span>{user.name} vam šalje zahtjev za prijateljstvo </span>
-                <span>Prihvati</span>
-                <span>Odbij</span>
-
-
-
+                <span>
+                    <span className="user-name" >{user.name} </span>
+                    vam šalje zahtjev za prijateljstvo </span>
+                <button type="submit" className="submitButton" onClick={() => confirm()}>
+                    Prihvati
+                </button>
+                <button type="submit" className="submitButton" onClick={() => refuse()}>
+                    Odbij
+                </button>
             </div>
+            ):(<div> </div>)}
         </div>
+
     );
 };
