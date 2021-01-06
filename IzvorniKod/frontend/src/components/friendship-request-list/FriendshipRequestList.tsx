ba22ca8218e79977707524a769be8815ebee206c
Luka Ravenscak <lravenscak@croz.net>
2021-01-06 01:43:32 +0100

    [#58] Refactored FriendshipRequestList.tsx

--- IzvorniKod/frontend/src/components/friendship-request-list/FriendshipRequestList.tsx
+++ IzvorniKod/frontend/src/components/friendship-request-list/FriendshipRequestList.tsx
@@ -1,39 +1,106 @@
-import React, {useEffect, useState} from "react";
-import {UserInfo} from "../mountain-lodge/models/UserInfo";
-import {HttpCodesUtil} from "../mountaineering-community/HttpCodesUtil";
-import {ShowFriendshipRequestList} from "./ShowFriendshipRequestList";
+import React, { useEffect, useState } from "react";
+import { UserInfo } from "../mountain-lodge/models/UserInfo";
+import { HttpCodesUtil } from "../mountaineering-community/HttpCodesUtil";
+import { useHistory } from "react-router";
 
 export const FriendshipRequestList = () => {
-    const [allUsers, setAllUsers] = useState<UserInfo[]>([]);
+  const [allUsers, setAllUsers] = useState<UserInfo[]>([]);
+  const history = useHistory();
 
-    useEffect(() => {
-        fetch("/api/users/friend-requests-received", {
-            method: "GET",
-            headers: new Headers({
-                authorization: sessionStorage.getItem("key") || "",
-            }),
-        }).then(function (response) {
-            if (response.status === HttpCodesUtil.SUCCESS) {
-                response.json().then((users) => {
-                    users.forEach(function (item: UserInfo) {
-                        item.image = "data:image/jpeg;base64," + item.image;
-                    });
-                    setAllUsers(users);
-                });
-            } else {
+  function confirm(user: UserInfo) {
+    fetch("api/users/friend-request-accept/" + user.id, {
+      method: "POST",
+      headers: new Headers({
+        authorization: sessionStorage.getItem("key") || "",
+        "Content-Type": "application/json",
+      }),
+    }).then((response) => {
+      if (response.status === 200) {
+        removeUser(user);
+      }
+    });
+  }
 
-            }
+  function refuse(user: UserInfo) {
+    fetch("api/users/friend-request-decline/" + user.id, {
+      method: "POST",
+      headers: new Headers({
+        authorization: sessionStorage.getItem("key") || "",
+        "Content-Type": "application/json",
+      }),
+    }).then((response) => {
+      if (response.status === 200) {
+        removeUser(user);
+      }
+    });
+  }
+
+  function removeUser(user: UserInfo) {
+    let users = allUsers;
+    users = users.filter((u) => u !== user);
+    setAllUsers(users);
+  }
+
+  useEffect(() => {
+    fetch("/api/users/friend-requests-received", {
+      method: "GET",
+      headers: new Headers({
+        authorization: sessionStorage.getItem("key") || "",
+      }),
+    }).then(function (response) {
+      if (response.status === HttpCodesUtil.SUCCESS) {
+        response.json().then((users) => {
+          users.forEach(function (item: UserInfo) {
+            item.image = "data:image/jpeg;base64," + item.image;
+          });
+          setAllUsers(users);
         });
-    }, []);
+      } else {
+      }
+    });
+  }, []);
 
-    return (
+  return (
+    <div>
+      {!allUsers.length ? (
+        <div>Nema pristiglih zahtjeva za prijateljstvo. </div>
+      ) : (
         <div>
-            {!allUsers.length ? (
-                <div>Nema pristiglih zahtjeva za prijateljstvo. </div>
-            ) : (
-                <ShowFriendshipRequestList allUsers={allUsers} setAllUsers={setAllUsers} />
-            )}
+          <span className="request-heading"> Zahtjevi za prijateljstvo </span>
+          {allUsers.map((user) => (
+            <div key={user.id}>
+              <div className="users-container">
+                <div>
+                  <img
+                    alt={user.name}
+                    src={user.image}
+                    className="user-photo"
+                    onClick={(e) => history.push("/profile/" + user.id)}
+                  />
+                  <span>
+                    <span className="user-name">{user.name} </span>
+                    vam šalje zahtjev za prijateljstvo{" "}
+                  </span>
+                  <button
+                    type="submit"
+                    className="submitButton"
+                    onClick={() => confirm(user)}
+                  >
+                    Prihvati
+                  </button>
+                  <button
+                    type="submit"
+                    className="submitButton"
+                    onClick={() => refuse(user)}
+                  >
+                    Odbij
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-    );
-
+      )}
+    </div>
+  );
 };
