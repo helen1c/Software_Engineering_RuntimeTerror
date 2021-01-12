95679381fa9e8a6fcc5d798d05659599dbd9a9a8
Ivan Martinović <ivan.martinovic@fer.hr>
2021-01-12 21:16:27 +0100

    [FINNALYLYLYLYYLY 2]

--- IzvorniKod/backend/src/main/java/hr/fer/pi/planinarskidnevnik/PlaninarskidnevnikApplication.java
+++ IzvorniKod/backend/src/main/java/hr/fer/pi/planinarskidnevnik/PlaninarskidnevnikApplication.java
@@ -2,12 +2,31 @@ package hr.fer.pi.planinarskidnevnik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PlaninarskidnevnikApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlaninarskidnevnikApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://planinarski-dnevnik-frontend.herokuapp.com",
+                                "http://localhost:3000",
+                                "https://frontend-planinarskidnevnik.herokuapp.com",
+                                "https://pdnevnik.herokuapp.com" )
+                        .allowedMethods("POST", "GET", "PATCH", "PUT", "DELETE").exposedHeaders("Authorization");
+            }
+        };
     }
 
 }
